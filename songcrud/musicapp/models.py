703e21c985385614806882d344267fb7c8df003c
Saint-Dave <babaloladavid7@gmail.com>
2022-11-06 14:34:15 +0100

    adding the attributes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -3,12 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Artiste(models.Model):
-    first_name = models.CharField(max_length=300) 
-    last_name = models.CharField(max_length=300)  
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100)  
     age = models.IntegerField() 
 
 class Song(models.Model):
-    pass
+    title = models.CharField() 
+    date_released = models.DateField() 
+    likes = models.ExpressionList
+    artiste_id = models.CharField()
+
 
 class Lyric(models.Model):
-    pass
+    content = models.CharField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)

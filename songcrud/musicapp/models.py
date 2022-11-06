0ec5abe2a347b2de4c71ae8910ca7c49b0801f74
Saint-Dave <babaloladavid7@gmail.com>
2022-11-06 18:17:12 +0100

    yes, done with this

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -7,12 +7,15 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=100) 
     last_name = models.CharField(max_length=100)  
     age = models.IntegerField() 
+    
+    def __str__(self):
+        return self.first_name
 
 class Song(models.Model):
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=300) 
-    date_released = models.DateField(default=datetime.today) 
-    likes = models.IntegerField()
+    date_released = models.DateField(default= datetime.today) 
+    likes = models.IntegerField()  
 
 class Lyric(models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
